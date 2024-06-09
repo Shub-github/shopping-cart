7808e91efe9e1f565010efa1e4ac201e56d9c741
SHUBHAM <swardshub@gmail.com>
2024-06-09 17:57:25 +0530

    alt_img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div>
       <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
         <NavLink to="/">
-          <img src="../logo.png" className="h-14" alt="logo" />
+          <img src="../logo.png" className="h-14" />
         </NavLink>
 
         <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
